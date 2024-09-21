23db93c05f81431570c4cea88aa990bced00be6e
Vipul11Bhatnagar <bhatnagarvipul5@gmail.com>
2024-09-21 18:42:12 +0530

    datatype summary

---
+++ 01_basics/data_types_summary.js
@@ -0,0 +1,54 @@
+//Primitive datatype---------------------------------------------------------------------------
+let name = "vipul" // string
+console.log(name)
+console.log(typeof name)
+
+let age= 27    // number
+console.log(age)
+console.log(typeof age)
+
+let isStudent= true  //boolean
+console.log(isStudent)
+console.log(typeof isStudent)
+
+
+let car; //undefined
+console.log(car)
+console.log(typeof car)
+
+let temprature = null // Null
+console.log(temprature)
+console.log(typeof temprature)
+
+
+
+
+// Non-Primitive datatype--------------------------------------------------------------------------------------------
+
+
+
+// object type
+let person =
+{
+name:"Vipul",
+age: 33,
+isStudent: true
+}
+
+console.log(person)
+
+//Array type
+let cars = [ "Hyryder", "fortuner", "creta" ];
+
+console.log(cars)
+
+//functions
+
+function greet() {
+    console.log("Hello, World!");
+
+}
+
+ greet();
+
+
