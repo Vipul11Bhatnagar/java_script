890d249a644cee6a3fd3bbba555a7145cd10b5d7
Vipul11Bhatnagar <bhatnagarvipul5@gmail.com>
2024-09-21 15:37:01 +0530

    datatype comparison

---
+++ 01_basics/datatype_comparisons.js
@@ -0,0 +1,18 @@
+// same datatype value compariosns
+
+console.log(2 >= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+
+//different dataype value comparison
+
+console.log("2" >= 1)
+console.log("2" != 1)
+
+
+//strict check ===
+// In this it will first check the datatype and then not allow to convert the datatype means if datatype are different then it will not allow to compare the values//
+
+console.log("2" === 1)
+
